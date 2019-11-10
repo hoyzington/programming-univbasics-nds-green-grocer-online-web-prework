de7e5d9d89ff4ffb2f78e0f157e753a297fb5b28
Scott Hoisington <hoyzington@gmail.com>
2019-11-10 04:56:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -86,10 +86,4 @@ def checkout(cart, coupons)
     total.round(2)
   end
   total
-  
-  
-  # This method should call: consolidate_cart, apply_coupons, apply_clearance
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers)
-  
 end
